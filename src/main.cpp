4cb717306ee5fe65de10a7f4d5dab38d21c70bb3
Daniel Streby <dstreby@2u.com>
2018-02-06 10:33:12 -0500

    Formatting / whitespace changes

--- src/main.cpp
+++ src/main.cpp
@@ -32,61 +32,61 @@ int adc_key_in  = 0;
 // read the buttons
 int read_LCD_buttons()
 {
- adc_key_in = analogRead(0);      // read the value from the sensor 
- // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
- // we add approx 50 to those values and check to see if we are close
- if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
- if (adc_key_in < 50)   return btnRIGHT;  
- if (adc_key_in < 195)  return btnUP; 
- if (adc_key_in < 380)  return btnDOWN; 
- if (adc_key_in < 555)  return btnLEFT; 
- if (adc_key_in < 790)  return btnSELECT;   
+	adc_key_in = analogRead(0);      // read the value from the sensor 
+	// my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+	// we add approx 50 to those values and check to see if we are close
+	if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+	if (adc_key_in < 50)   return btnRIGHT;  
+	if (adc_key_in < 195)  return btnUP; 
+	if (adc_key_in < 380)  return btnDOWN; 
+	if (adc_key_in < 555)  return btnLEFT; 
+	if (adc_key_in < 790)  return btnSELECT;   
 
- return btnNONE;  // when all others fail, return this...
+	return btnNONE;  // when all others fail, return this...
 }
 
 void printtemp_set()
 {
- lcd.setCursor(6,1);            // move cursor to second line "1" and 6 spaces over
- lcd.print(temp_set);
- lcd.write('   ');              // Super lazy way to make sure the line is cleared after the temp_set
+	lcd.setCursor(6,1);            // move cursor to second line "1" and 6 spaces over
+	lcd.print(temp_set);
+	lcd.write('   ');              // Super lazy way to make sure the line is cleared after the temp_set
 }
 
 void readtemp()
 {
-  temp = round(dht.readTemperature(READ_F));
-  humid = round(dht.readHumidity());
+	temp = round(dht.readTemperature(READ_F));
+	humid = round(dht.readHumidity());
 }
 
 void printtemp()
 {
-  lcd.setCursor(6,0);
-  lcd.print(temp);
-  lcd.print("F ");
-  lcd.print(humid);
-  lcd.print("%   ");
+	lcd.setCursor(6,0);
+	lcd.print(temp);
+	lcd.print("F ");
+	lcd.print(humid);
+	lcd.print("%   ");
 }
 
 void setrelay()
 {
-  lcd.setCursor(10,1);
-  if (temp_set > temp) {
-    relay_state = 1;
-    digitalWrite(RELAYPIN, HIGH);
-    lcd.print("ON ");
-  } else {
-    relay_state = 0;
-    digitalWrite(RELAYPIN, LOW);
-    lcd.print("OFF");
-  }
+	lcd.setCursor(10,1);
+	if (temp_set > temp) {
+		relay_state = 1;
+		digitalWrite(RELAYPIN, HIGH);
+		lcd.print("ON ");
+	} else {
+		relay_state = 0;
+		digitalWrite(RELAYPIN, LOW);
+		lcd.print("OFF");
+	}
 }
 
 void init_display()
 {
- lcd.setCursor(0,0);
- lcd.print("Temp: ");
- lcd.setCursor(0,1);
- lcd.print("Set: ");
+	lcd.setCursor(0,0);
+	lcd.print("Temp: ");
+	lcd.setCursor(0,1);
+	lcd.print("Set: ");
 }
 
 void set_backlight()
@@ -103,68 +103,68 @@ void set_backlight()
 
 void setup()
 {
- pinMode(RELAYPIN, OUTPUT);
- digitalWrite(RELAYPIN, LOW);
- lcd.begin(16, 2);              // start the library
- dht.begin();
- init_display();
- pinMode(BACKLIGHT_PIN, OUTPUT);
- set_backlight();
+	pinMode(RELAYPIN, OUTPUT);
+	digitalWrite(RELAYPIN, LOW);
+	lcd.begin(16, 2);              // start the library
+	dht.begin();
+	init_display();
+	pinMode(BACKLIGHT_PIN, OUTPUT);
+	set_backlight();
 }
- 
+
 void loop()
 {
- readtemp();
- printtemp();
- printtemp_set();
- setrelay();
+	readtemp();
+	printtemp();
+	printtemp_set();
+	setrelay();
 
- lcd_key = read_LCD_buttons();  // read the buttons
+	lcd_key = read_LCD_buttons();  // read the buttons
 
- switch (lcd_key)               // depending on which button was pushed, we perform an action
- {
-   case btnRIGHT:
-     {
-     backlight += 16;
-     set_backlight();
-     delay(BTNDELAY);
-     break;
-     }
-   case btnLEFT:
-     {
-     backlight -= 16;
-     set_backlight();
-     delay(BTNDELAY);
-     break;
-     }
-   case btnUP:
-     {
-     //lcd.print("UP    ");
-     temp_set++;
-     printtemp_set();
-     delay(BTNDELAY);
-     break;
-     }
-   case btnDOWN:
-     {
-     //lcd.print("DOWN  ");
-     temp_set--;
-     printtemp_set();
-     delay(BTNDELAY);
-     break;
-     }
-   case btnSELECT:
-     {
-     //lcd.print("SELECT");
-     break;
-     }
-    /*
-   case btnNONE:
-     {
-     lcd.print("NONE  ");
-     break;
-     }
-     */
- }
+	switch (lcd_key)               // depending on which button was pushed, we perform an action
+	{
+		case btnRIGHT:
+			{
+				backlight += 16;
+				set_backlight();
+				delay(BTNDELAY);
+				break;
+			}
+		case btnLEFT:
+			{
+				backlight -= 16;
+				set_backlight();
+				delay(BTNDELAY);
+				break;
+			}
+		case btnUP:
+			{
+				//lcd.print("UP    ");
+				temp_set++;
+				printtemp_set();
+				delay(BTNDELAY);
+				break;
+			}
+		case btnDOWN:
+			{
+				//lcd.print("DOWN  ");
+				temp_set--;
+				printtemp_set();
+				delay(BTNDELAY);
+				break;
+			}
+		case btnSELECT:
+			{
+				//lcd.print("SELECT");
+				break;
+			}
+			/*
+				 case btnNONE:
+				 {
+				 lcd.print("NONE  ");
+				 break;
+				 }
+				 */
+	}
 
 }
