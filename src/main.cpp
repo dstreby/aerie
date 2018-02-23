f433c6a040a8155cde0ce3c9b404c30a0b387ba9
Daniel Streby <dstreby@2u.com>
2018-02-22 21:06:55 -0500

    Adding preprocessor macros to allow for compile time change to display module version
    * Refactored read_LCD_buttons to use macros
    * Defined default macro values for display v1
    * Define optional macro values for display v2
    * Refactored platformio config with prod and stg envs

--- src/main.cpp
+++ src/main.cpp
@@ -37,14 +37,12 @@ void set_backlight()
 int read_LCD_buttons()
 {
 	adc_key_in = analogRead(0);      // read the value from the sensor 
-	// my buttons when read are centered at these valies: 0, 144, 329, 504, 741
-	// we add approx 50 to those values and check to see if we are close
-	if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
-	if (adc_key_in < 50)   return btnRIGHT;
-	if (adc_key_in < 195)  return btnUP;
-	if (adc_key_in < 380)  return btnDOWN;
-	if (adc_key_in < 555)  return btnLEFT;
-	if (adc_key_in < 790)  return btnSELECT;
+	if (adc_key_in > THRESH_NONE)		return btnNONE; // Most likely result first
+	if (adc_key_in < THRESH_RIGHT)	return btnRIGHT;
+	if (adc_key_in < THRESH_UP)			return btnUP;
+	if (adc_key_in < THRESH_DOWN)		return btnDOWN;
+	if (adc_key_in < THRESH_LEFT)		return btnLEFT;
+	if (adc_key_in < THRESH_SELECT)	return btnSELECT;
 
 	return btnNONE;  // when all others fail, return this...
 }
